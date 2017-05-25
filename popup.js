a8522c8757b43d2b241f5cebd37567a17b1c57f2
Bill Kim <munsungk@andrew.cmu.edu>
2017-05-25 19:46:32 -0400

    Gets info from background.js. No able to get lyrics yet

--- popup.js
+++ popup.js
@@ -2,21 +2,14 @@
  * Created by bill on 2017-05-25.
  */
 
-(function() {
+chrome.runtime.sendMessage({from: "popup"}, function(data) {
+    console.log(data);
 
-    let trackName = "";
-    let trackArtist = "";
-
-    chrome.runtime.onMessage.addListener(
-        function(request, sender, sendResponse) {
-            if (request.text) {
-                trackName = request.name;
-                trackArtist = request.artist;
-            }
-        }
-    );
-
-    console.log(trackName);
-    console.log(trackArist);
-
-})();
+    let xmlHttp = new XMLHttpRequest();
+    let url = "https://genius.com/" + data.artist + "-" + data.name + "-lyrics";
+    url = url.replace(/ /g, "-");
+    console.log(url);
+    xmlHttp.open("GET", "https://genius.com/Childish-gambino-redbone-lyrics", true);
+    xmlHttp.send();
+    // document.getElementById("lyrics").innerHTML = xmlHttp.responseText;
+});
