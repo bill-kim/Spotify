78ad21b7d90e2df0267852d825fc86a0873292e1
Bill Kim <munsungk@andrew.cmu.edu>
2017-05-25 17:57:16 -0400

    Sends info to background instead of popup

--- content.js
+++ content.js
@@ -2,13 +2,29 @@
  * Created by bill on 2017-05-25.
  */
 
-(function() {
-    let track_info = document.getElementsByClassName("track-info__name").getElementsByTagName("a");
+let id = setInterval(checkLoaded, 300);
+
+function checkLoaded() {
+    if (document.getElementsByClassName('track-info__name').length === 0) {
+        return;
+    }
+    else {
+        clearInterval(id);
+        fullyLoaded();
+    }
+}
+
+function fullyLoaded() {
+    let track_name = document.getElementsByClassName('track-info__name')[0].firstChild.firstChild.innerHTML;
+    let track_artist = document.getElementsByClassName('track-info__artists')[0].firstChild.firstChild.firstChild.innerHTML;
 
     let info = {
-        name: track_info[0],
-        artist: track_info[1]
-    }
+        name: track_name,
+        artist: track_artist
+    };
+
+    console.log(info);
 
     chrome.runtime.sendMessage(info, null);
-})();
+}
+
